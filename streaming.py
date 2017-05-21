f940c5aa1d532ff90b5f8c6bfeaaf233558fc25a
a-lilas <a-simada@ist.osaka-u.ac.jp>
2017-05-21 21:07:50 +0900

    [add] ユーザのタイムラインからワードクラウドを生成する機能

--- streaming.py
+++ streaming.py
@@ -20,18 +20,36 @@ class Listener(tweepy.StreamListener):
         if str(status.in_reply_to_screen_name) == secret.MY_USER_ID:
             # ツイートから，@以下を削除し，対象単語のみを抽出
             searchword = re.sub(r'^@.+? ', '', status.text)
-            # 空白で，リストに分割
-            searchword_list = searchword.split()
+
             # filename:時刻を名前にする
             filename_time = str(datetime.datetime.today())
 
+            # ツイート簡易操作クラス作成
             tw = makewc.TwitterOperator()
-            tw.searchWord(searchword_list)
+
+            if searchword != '@'+secret.MY_USER_ID:
+                # 単語が，ユーザ名と一致しない -> 検索単語が含まれている
+                # 空白で，リストに分割
+                searchword_list = searchword.split()
+                # 入力された単語についてツイート検索
+                tw.searchWord(searchword_list)
+            else:
+                # リプライ内に検索単語が含まれない
+                # リプライしてきたユーザのタイムラインを取得
+                searchword_list = ['']
+                tw.getTimeline(get_id=status.user.screen_name)
+
+            # ワードクラウド作成
             makewc.makecloud(tw, './wordcloud_image/' + filename_time + '.png')
+
             # ツイート内容を以下の変数に記述
             tweet = '.@' + status.user.screen_name + ' ' + ' '.join(searchword_list) + ' ' + filename_time \
                                     + ' #wordcloud #ワードクラウド'
-            twpy.api.update_with_media(filename='./wordcloud_image/'+filename_time+'.png', status=tweet)
+            # 画像を添付してツイート
+            twpy.api.update_with_media(filename='./wordcloud_image/'+filename_time+'.png',
+                                       status=tweet,
+                                       in_reply_to_status_id=status.id
+                                       )
 
             # ログ出力
             f.write(' '.join(searchword_list) + ' ' + filename_time + '\n')
